4589770a3e855d780241760c56362697cd708b77
mrkmj <31210425+mrkmj@users.noreply.github.com>
2017-10-28 15:17:36 +0800

    Create You're a square.py

---
+++ You're a square.py
@@ -0,0 +1,39 @@
+#判断一个数是否为平方数
+
+'''
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+
+is_square (-1) # => false
+is_square   3 # => false
+is_square   4 # => true
+is_square  25 # => true
+is_square  26 # => false
+
+'''
+
+#我的方法
+#1050ms
+def is_square(n):
+  i = 1
+  if n < 0:
+    return False
+  else:
+    while n!=0 and n>0:
+      n -= i
+      i+=2
+    if n == 0:
+      return True
+    else:
+      return False
+      
+#别人的高支持方法
+#203ms
+from math import sqrt
+
+def is_square(n):
+    return n > 0 and sqrt(n).is_integer()

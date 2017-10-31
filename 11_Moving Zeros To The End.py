7ff062b3a3c6ad2c1c785e3463b2aca04b35702e
mrkmj <31210425+mrkmj@users.noreply.github.com>
2017-11-01 00:52:48 +0800

    Create 11_Moving Zeros To The End.py

---
+++ 11_Moving Zeros To The End.py
@@ -0,0 +1,35 @@
+'''
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+move_zeros([false,1,0,1,2,0,1,3,"a"]) # returns[false,1,1,2,1,3,"a",0,0]
+
+ps：大坑啊
+Test.assert_equals(move_zeros([1,2,0,1,0,1,0,3,0,1]),[ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
+Test.assert_equals(move_zeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]),[9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+Test.assert_equals(move_zeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9]),["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+Test.assert_equals(move_zeros(["a",0,0,"b",None,"c","d",0,1,False,0,1,0,3,[],0,1,9,0,0,{},0,0,9]),["a","b",None,"c","d",1,False,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0])
+Test.assert_equals(move_zeros([0,1,None,2,False,1,0]),[1,None,2,False,1,0,0])
+
+要处理False，0.0……
+'''
+
+#My code
+def move_zeros(array):
+    result = []
+    count = 0
+    for i in range(len(array)):
+        if type(array[i]) == float:
+            array[i] = int(array[i])
+        array[i] = int(array[i])
+        if array[i] is not 0:
+            result.append(array[i])
+        else:
+            count += 1
+    for i in range(count):
+        result.append(0)
+    return result
+    
+#Better code
+def move_zeros(arr):
+    l = [i for i in arr if isinstance(i, bool) or i!=0]
+    return l+[0]*(len(arr)-len(l))

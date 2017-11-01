d86d66453c14424bb71acd5efdfc6562e611915a
mrkmj <31210425+mrkmj@users.noreply.github.com>
2017-11-01 18:06:01 +0800

    Create 12_WeIrD StRiNg CaSe.py

---
+++ 12_WeIrD StRiNg CaSe.py
@@ -0,0 +1,20 @@
+'''
+Write a function toWeirdCase (weirdcase in Ruby) that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+
+to_weird_case('String'); # => returns 'StRiNg'
+to_weird_case('Weird string case') # => returns 'WeIrD StRiNg CaSe'
+'''
+
+#My code
+def to_weird_case(string):
+    midddle = string.split()
+    for j in range(len(midddle)):
+        change = list(midddle[j])
+        for i in range(0,len(change),2):
+            change[i] = change[i].upper()
+            midddle[j] = ''.join(change)
+    return ' '.join(midddle)
